1dcc64d71f10ac01e145ffabbff40f17444c8203
Chad <Chad@CBSKIT>
2019-02-22 15:54:44 -0700

    Creation of initial version of calculator and documentation.

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,62 @@
+/*
+ *@Author: Chad Snyder
+ *@Date:   02/21/2018
+ * Class:  CSE360
+ * ID:     553
+ */
+
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	/* @param value
+	 * The add method should add the
+	 * parameter to the total variable
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/* @param value
+	 * The subtract method should subtract
+	 * the parameter to the total variable
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/* @param value
+	 * The multiply method should multiply the
+	 * total by the parameter
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/* @param value
+	 * The divide method should divide the
+	 * total by the parameter.
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/* 
+	 * @param
+	 * @return string
+	 * 
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
